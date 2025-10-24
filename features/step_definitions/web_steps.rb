72db0fcbd28a31e59c95da87e5543b51acf7693b
fornari03 <221017023@aluno.unb.br>
2025-10-24 20:14:28 -0300

    update: add .features for testing

---
+++ features/step_definitions/web_steps.rb
@@ -0,0 +1,32 @@
+Given /^(?:|I )am on (.+)$/ do |page_name|
+  visit path_to(page_name)
+end
+
+When /^(?:|I )press "([^"]*)"$/ do |button|
+  click_button(button)
+end
+
+When /^(?:|I )follow "([^"]*)"$/ do |link|
+  click_link(link)
+end
+
+When /^(?:|I )check "([^"]*)"$/ do |field|
+  check(field)
+end
+
+When /^(?:|I )uncheck "([^"]*)"$/ do |field|
+  uncheck(field)
+end
+
+Then /^(?:|I )should see "([^"]*)"$/ do |text|
+  expect(page).to have_content(text)
+end
+
+Then /^(?:|I )should not see "([^"]*)"$/ do |text|
+  expect(page).not_to have_content(text)
+end
+
+Then /^I should be on (.+)$/ do |page_name|
+  current_path = URI.parse(current_url).path
+  expect(current_path).to eq(path_to(page_name))
+end
