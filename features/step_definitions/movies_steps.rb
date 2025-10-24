f675d6ec7f00c38fb0bcca1fc4bb21632d768450
fornari03 <221017023@aluno.unb.br>
2025-10-24 20:25:10 -0300

    feat: implements step to check multiple ratings and step to check if all movies are visible

--- features/step_definitions/movies_steps.rb
+++ features/step_definitions/movies_steps.rb
@@ -4,3 +4,22 @@ Given "the following movies exist:" do |movies_table|
     Movie.create!(movie)
   end
 end
+
+# step to check multiple ratings
+When /I (check|uncheck) the following ratings: (.*)/ do |action, ratings_list|
+  ratings = ratings_list.split(', ')
+  ratings.each do |rating|
+    field_name = "ratings[#{rating}]"
+    if action == "check"
+      check(field_name)
+    else
+      uncheck(field_name)
+    end
+  end
+end
+
+# step to check if all movies are visible
+Then "I should see all of the movies" do
+  rows = page.all('tbody tr').size
+  expect(rows).to eq(Movie.count)
+end
