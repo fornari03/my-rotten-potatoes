24c3e3ab4f18d6bd592c1a7edcd22312210dea44
fornari03 <221017023@aluno.unb.br>
2025-10-24 21:54:24 -0300

    feat: add ratings to movie controller and view

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,12 +1,19 @@
 class MoviesController < ApplicationController
+  
+  helper_method :highlight_class
+
   def index
+    @all_ratings = Movie.all_ratings
+    
+    if params[:ratings]
+      @ratings_to_show = params[:ratings].keys
+    else
+      @ratings_to_show = @all_ratings
+    end
+    
     @sort_by = params[:sort]
 
-    if @sort_by
-      @movies = Movie.order(@sort_by)
-    else
-      @movies = Movie.all
-    end
+    @movies = Movie.where(rating: @ratings_to_show).order(@sort_by)
   end
 
   def show
@@ -22,7 +29,8 @@ class MoviesController < ApplicationController
     if @movie.save
       redirect_to movies_path, notice: "#{@movie.title} foi criado com sucesso."
     else
-      render 'new'
+      flash.now[:alert] = "Não foi possível criar o filme."
+      render 'new', status: :unprocessable_entity
     end
   end
 
@@ -35,18 +43,22 @@ class MoviesController < ApplicationController
     if @movie.update(movie_params)
       redirect_to movie_path(@movie), notice: "#{@movie.title} foi atualizado com sucesso."
     else
-      render 'edit'
+      flash.now[:alert] = "Não foi possível atualizar o filme."
+      render 'edit', status: :unprocessable_entity
     end
   end
 
   def destroy
     @movie = Movie.find(params[:id])
     @movie.destroy
-    redirect_to movies_path, notice: "#{@movie.title} foi deletado com sucesso."
+    redirect_to movies_path, notice: "'#{@movie.title}' foi deletado com sucesso."
   end
 
-  
-  private # define que os métodos abaixo são privados
+  private
+
+  def highlight_class(column_name)
+    'bg-yellow-200' if @sort_by == column_name
+  end
 
   def movie_params
     params.require(:movie).permit(:title, :rating, :description, :release_date)
